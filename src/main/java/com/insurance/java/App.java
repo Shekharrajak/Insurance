ae984b5359a49390df55fdac6e6e54c3ea6b512e
shekharrajak <shekharrajak@live.com>
2017-10-06 14:53:24 +0530

    dependency added logger

--- src/main/java/com/insurance/java/App.java
+++ src/main/java/com/insurance/java/App.java
@@ -11,9 +11,22 @@ public class App
 //        System.out.println( "Hello World!" );
 //    }
 
+   /* Get actual class name to be printed on */
+   static Logger log = Logger.getLogger(log4jExample.class.getName());
+     log.debug("Hello this is a debug message");
+	 log.info("Hello this is an info messag");
     public static void main(String[] args){
         ClaimApprovalManager cam = new ClaimApprovalManager();
         cam.processClaim(new HealthInsuranceSurveyor());
     }
 }
 
+/*
+
+mvn clean
+mvn compile
+mvn package
+java -cp target/Insurance-1.0-SNAPSHOT.jar main.java.com.insurance.java.App
+
+
+*/
